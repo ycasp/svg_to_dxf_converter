93263d638cbd469ef1ddfc53f2cace5e12dfec41
yannik <yannik.caspar@unibas.ch>
2025-02-26 11:09:28 +0100

    update for folder ignoring from gitignore

---
+++ src/logging_config.py
@@ -0,0 +1,22 @@
+import logging
+
+LOG_FORMAT = "%(asctime)s:%(name)s:%(levelname)s:%(pathname)s:%(lineno)d:%(message)s"
+
+def setup_logger(name,log_file = "swissmade365.log", log_level = logging.DEBUG):
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+
+    # prevent duplicate
+    if logger.hasHandlers():
+        return logger
+
+    # console handler
+
+    # file handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(log_level)
+    file_formatter = logging.Formatter(LOG_FORMAT)
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+
+    return logger
