25fdc2e606cb271f6d3c6ca4e80724953e8ae879
yannik <yannik.caspar@unibas.ch>
2025-02-28 08:24:49 +0100

    built in cut rules step

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from logging_config import setup_logger
+from src.cut_rules import enforce_cut_rules
 from src.dxf_handler import write_dxf
-from src.svg_handler import read_svg_file, scale_file
+from src.svg_handler import read_svg_file, scale_file, scale_file_param
 
 main_logger = setup_logger(__name__)
 
@@ -17,15 +18,19 @@ if __name__ == "__main__":
     # /fruits and veggies/
     # /dohner ag/
     filepath = "C:/Users/ycasp/Documents/Projekt Lukas/svg collection/basic geometric forms/"
-    filename = "triangle-half"
+    filename = "cut_rules"
     ending = ".svg"
     try:
         # read in svg (xml) file into tree
         svg_root = read_svg_file(filepath + filename + ending)
 
         # scale file
-        svg_root = scale_file(svg_root, 266.520, 373.335)
-        # svg_root = scale_file_param(svg_root, 2, 2)
+        # svg_root = scale_file(svg_root, 266.520, 373.335)
+        scale = 1
+        svg_root = scale_file_param(svg_root, scale, scale)
+
+        # cut rules
+        enforce_cut_rules(svg_root, 2)
 
         # write svg content to dxf file and save it
         write_dxf(svg_root, filename)
