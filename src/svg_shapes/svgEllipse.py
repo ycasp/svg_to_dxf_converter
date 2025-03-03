ac41c67c8285f57dc4b9e3ca420bc0b8911a3012
yannik <yannik.caspar@unibas.ch>
2025-03-03 15:26:51 +0100

    added rotation to svgEllipse.py and getter for name + wrote test for initialization and getter

--- src/svg_shapes/svgEllipse.py
+++ src/svg_shapes/svgEllipse.py
@@ -1,10 +1,28 @@
+from src.utilities import change_svg_to_dxf_coordinate, export_rotation, rotate_clockwise_around_point
+
 
 class SvgEllipse:
 
     def __init__(self, element, svg_height):
         self.name = 'ellipse'
+
         self.center_x = float(element.get('cx'))
-        self.center_y = float(element.get('cy'))
-        self.radius_x = float(element.get('rx'))
-        self.radius_y = float(element.get('ry'))
+        self.center_y = change_svg_to_dxf_coordinate(float(element.get('cy')), svg_height)
+
+        self.radius_x = (float(element.get('rx')), 0)
+        self.radius_y = (0, float(element.get('ry')))
+
         self.transform = element.get('transform')
+        if self.transform is not None:
+            rotation = export_rotation(self.transform)
+            if rotation is not None:
+                self.center_x, self.center_y = rotate_clockwise_around_point(self.center_x, self.center_y,
+                                        rotation[0], rotation[1], change_svg_to_dxf_coordinate(rotation[2], svg_height))
+                self.radius_x = rotate_clockwise_around_point(self.radius_x[0], self.radius_x[1],
+                                        rotation[0], 0, 0)
+                self.radius_y = rotate_clockwise_around_point(self.radius_y[0], self.radius_y[1],
+                                        rotation[0], 0, 0)
+
+
+    def get_ellipse_name(self):
+        return self.name
---
+++ tests/tests_svg-shapes/test_svgEllipse.py
@@ -0,0 +1,33 @@
+import unittest
+
+from src.svg_shapes.svgEllipse import *
+
+class TestSvgEllipse(unittest.TestCase):
+    def setUp(self):
+        self.normal_ellipse = {'cx':"70", 'cy':"50", 'rx':"60", 'ry':"40"}
+        self.rotated_ellipse = {'cx':"70", 'cy':"50", 'rx':"60", 'ry':"40", 'transform': 'rotate(-90)'}
+        self.svg_height = 100
+
+    def test_initialization(self):
+        normal_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertEqual(normal_ellipse.center_x, 70)
+        self.assertEqual(normal_ellipse.center_y, 50)
+        self.assertEqual(normal_ellipse.radius_x, (60, 0))
+        self.assertEqual(normal_ellipse.radius_y, (0, 40))
+        self.assertEqual(normal_ellipse.transform, None)
+        self.assertEqual(normal_ellipse.name, 'ellipse')
+
+        rot_ellipse = SvgEllipse(self.rotated_ellipse, self.svg_height)
+        self.assertEqual(rot_ellipse.center_x, 50)
+        self.assertEqual(rot_ellipse.center_y, 170)
+        self.assertEqual(rot_ellipse.radius_x, (0, 60))
+        self.assertEqual(rot_ellipse.radius_y, (-40, 0))
+        self.assertEqual(rot_ellipse.transform, 'rotate(-90)')
+        self.assertEqual(rot_ellipse.name, 'ellipse')
+
+    def test_getters(self):
+        normal_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertEqual(normal_ellipse.get_ellipse_name(), 'ellipse')
+
+        rot_ellipse = SvgEllipse(self.rotated_ellipse, self.svg_height)
+        self.assertNotEqual(rot_ellipse.get_ellipse_name(), 'circle')
