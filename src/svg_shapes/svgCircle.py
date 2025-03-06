a88a7fde7897f162a34213d8545ff88978aff9c9
yannik <yannik.caspar@unibas.ch>
2025-03-06 17:27:43 +0100

    implemented the whole transform message and tested it

--- src/svg_shapes/svgCircle.py
+++ src/svg_shapes/svgCircle.py
@@ -1,5 +1,9 @@
-from src.utilities import export_rotation, change_svg_to_dxf_coordinate, rotate_clockwise_around_point
+from src.svg_shapes.transform_messages import export_transformations
+from src.utilities import change_svg_to_dxf_coordinate, rotate_clockwise_around_point, \
+    translate_coordinate, scale_coordinate, matrix_transformation
+from src.logging_config import setup_logger
 
+svg_circle_logger = setup_logger('svg_circle')
 
 class SvgCircle:
 
@@ -10,17 +14,15 @@ class SvgCircle:
         self.center_x = float(segment.get('cx'))
         self.center_y = change_svg_to_dxf_coordinate(float(segment.get('cy')), svg_height)
 
-        # extract transformations
-        self.transform = segment.get('transform')
-        if self.transform is not None:
-            # rotate if necessary
-            rotation = export_rotation(self.transform)
-            if rotation is not None:
-                    self.center_x, self.center_y = rotate_clockwise_around_point(self.center_x, self.center_y,
-                                            rotation[0], rotation[1], change_svg_to_dxf_coordinate(rotation[2], svg_height))
-
         # extract radius
         self.radius = float(segment.get('r'))
+        self.radius_y = 0
+        # extract transformations
+        transform_message = segment.get('transform')
+        if transform_message is not None:
+            self.transformation_list = export_transformations(transform_message)
+            self.transform(svg_height)
+
 
     def get_name(self):
         return self.name
@@ -29,3 +31,44 @@ class SvgCircle:
         self.center_x = self.center_x * scale_x
         self.center_y = self.center_y * scale_y
         self.radius = self.radius * scale_x
+        # self.radius_y = self.radius_y * scale_y
+
+    def transform(self, svg_height):
+        for t_type, values in self.transformation_list:
+            match t_type:
+                case 'translate':
+                    self.center_x = translate_coordinate(self.center_x, values[0])
+                    if len(values) > 1:
+                        self.center_y = translate_coordinate(self.center_y, values[1])
+                case 'rotate':
+                    if len(values) == 3:
+                        self.center_x, self.center_y = rotate_clockwise_around_point(self.center_x, self.center_y,
+                                                    values[0], values[1], change_svg_to_dxf_coordinate(values[2], svg_height))
+                    elif len(values) == 1:
+                        self.center_x, self.center_y = rotate_clockwise_around_point(self.center_x, self.center_y,
+                                                                                     values[0], 0, svg_height)
+                    else:
+                        svg_circle_logger.warning(f"unknown rotation message, value length: {len(values)}")
+                case 'scale':
+                    # self.center_x = scale_coordinate(self.center_x, values[0])
+                    r = self.radius
+                    self.radius = scale_coordinate(self.radius, values[0])
+                    if len(values) > 1:
+                        # self.center_y = scale_coordinate(self.center_y, values[1])
+                        self.radius_y = scale_coordinate(r, values[1])
+                    else:
+                        # self.center_y = scale_coordinate(self.center_y, values[0])
+                        pass
+                case 'skewX':
+                    svg_circle_logger.warning("skewX in circle - would give an ellipse, not handled")
+                case 'skewY':
+                    svg_circle_logger.warning("skewY in circle - would give an ellipse, not handled")
+                case 'matrix':
+                    if len(values) == 6:
+                        self.center_x, self.center_y = matrix_transformation(self.center_x, self.center_y, values)
+                    elif len(values) == 4:
+                        values.append(0)
+                        values.append(0)
+                        self.center_x, self.center_y = matrix_transformation(self.center_x, self.center_y, values)
+                    else:
+                        svg_circle_logger.warning(f"unknown matrix message, values length: {len(values)}")
--- tests/tests_svg-shapes/test_svgCircle.py
+++ tests/tests_svg-shapes/test_svgCircle.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ezdxf.render.forms import circle
+from numpy.random import normal
 
 from src.svg_shapes.svgCircle import *
 
@@ -18,14 +18,13 @@ class TestSvgCircle(unittest.TestCase):
         self.assertEqual(circle.center_x, 100)
         self.assertEqual(circle.center_y, 200)
         self.assertEqual(circle.radius, 50)
-        self.assertEqual(circle.transform, None)
 
         rot_circle = SvgCircle(self.rot_circle, self.svg_height)
         self.assertEqual(rot_circle.name, 'circle')
         self.assertEqual(rot_circle.center_x, -100)
         self.assertEqual(rot_circle.center_y, 200)
         self.assertEqual(rot_circle.radius, 50)
-        self.assertNotEqual(rot_circle.transform, None)
+        self.assertNotEqual(rot_circle.transformation_list, ['rotate', [90]])
 
     def test_getters(self):
         normal_circle = SvgCircle(self.normal_circle, self.svg_height)
@@ -33,3 +32,76 @@ class TestSvgCircle(unittest.TestCase):
 
         rot_circle = SvgCircle(self.rot_circle, self.svg_height)
         self.assertEqual(rot_circle.get_name(), 'circle')
+
+    def test_scale(self):
+        scale = 3
+        circle = SvgCircle(self.normal_circle, self.svg_height)
+        circle.scale(scale, scale)
+        self.assertEqual(circle.name, 'circle')
+        self.assertEqual(circle.center_x, 300)
+        self.assertEqual(circle.center_y, 600)
+        self.assertEqual(circle.radius, 150)
+
+        scale_x = 0.5
+        scale_y = 0.25
+        rot_circle = SvgCircle(self.rot_circle, self.svg_height)
+        rot_circle.scale(scale_x, scale_y)
+        self.assertEqual(rot_circle.name, 'circle')
+        self.assertEqual(rot_circle.center_x, -50)
+        self.assertEqual(rot_circle.center_y, 50)
+        self.assertEqual(rot_circle.radius, 25)
+        self.assertNotEqual(rot_circle.transformation_list, ['rotate', [90]])
+
+    def test_transform(self):
+        self.normal_circle['transform'] = 'translate(40)'
+        x_trans_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(x_trans_circle.center_x, 140)
+        self.assertEqual(x_trans_circle.center_y, 200)
+        self.assertEqual(x_trans_circle.radius, 50)
+
+        self.normal_circle['transform'] = 'translate(40, 150)'
+        trans_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(trans_circle.center_x, 140)
+        self.assertEqual(trans_circle.center_y, 350)
+        self.assertEqual(trans_circle.radius, 50)
+
+        self.normal_circle['transform'] = 'rotate(90)'
+        rot_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(rot_circle.center_x, -100)
+        self.assertEqual(rot_circle.center_y, 200)
+        self.assertEqual(rot_circle.radius, 50)
+
+        self.normal_circle['transform'] = 'rotate(-90, 100, 100)'
+        rot_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(rot_circle.center_x, 100)
+        self.assertEqual(rot_circle.center_y, 200)
+        self.assertEqual(rot_circle.radius, 50)
+
+        self.normal_circle['transform'] = 'scale(0.5)'
+        scaled_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(scaled_circle.radius, 25)
+        self.assertEqual(scaled_circle.radius_y, 0)
+
+        self.normal_circle['transform'] = 'scale(0.5, 3)'
+        scaled_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(scaled_circle.radius, 25)
+        self.assertEqual(scaled_circle.radius_y, 150)
+
+        self.normal_circle['transform'] = 'matrix(1, 0, 0, 1, 30, -30)'
+        matrix_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(matrix_circle.center_x, 130)
+        self.assertEqual(matrix_circle.center_y, 170)
+        self.assertEqual(matrix_circle.radius, 50)
+
+        self.normal_circle['transform'] = 'matrix(0, 1, 1, 0)'
+        matrix_circle = SvgCircle(self.normal_circle, self.svg_height)
+        self.assertEqual(matrix_circle.center_x, 200)
+        self.assertEqual(matrix_circle.center_y, 100)
+        self.assertEqual(matrix_circle.radius, 50)
+
+        crazy_trafo_element = {'cx': '0', 'cy': '0', 'r': '1',
+            'transform': 'rotate(90, 1, 0) translate(3, 5) matrix(0, -1, -1, 0) matrix(1, 0, 0,  1, -4, -6) scale(3)'}
+        crazy_circle = SvgCircle(crazy_trafo_element, 0)
+        self.assertEqual(crazy_circle.center_x, -10)
+        self.assertEqual(crazy_circle.center_y, -10)
+        self.assertEqual(crazy_circle.radius, 3)
