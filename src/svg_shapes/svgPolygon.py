79ce3f1254fcb926bb1ad041d815d6b595c3c75a
yannik <yannik.caspar@unibas.ch>
2025-03-10 18:08:22 +0100

    added transformation for svg Polygon and tested it

--- src/svg_shapes/svgPolygon.py
+++ src/svg_shapes/svgPolygon.py
@@ -1,14 +1,25 @@
-from src.utilities import change_svg_to_dxf_coordinate
+from src.utilities import change_svg_to_dxf_coordinate, translate_coordinate, rotate_clockwise_around_point, \
+    scale_coordinate, skew_x, skew_y
+from src.svg_shapes.transform_messages import export_transformations
+from src.logging_config import setup_logger
 
+svg_polygon_logger = setup_logger(__name__)
 
 class SvgPolygon:
 
     def __init__(self, element, svg_height):
         self.name = 'polygon'
         points = element.get('points')
-        self.point_list = [(float(p.split(',')[0]), change_svg_to_dxf_coordinate(float(p.split(',')[1]), svg_height))
+        self.point_list = [(float(p.split(',')[0]), float(p.split(',')[1]))
             for p in points.strip().split()]
-        self.transform = element.get('transform')
+        transform_message = element.get('transform')
+        if transform_message is not None:
+            self.transformation_list = export_transformations(transform_message)
+            self.transform()
+
+        for i in range (0, len(self.point_list)):
+            self.point_list[i] = (self.point_list[i][0], change_svg_to_dxf_coordinate(self.point_list[i][1], svg_height))
+
 
     def get_name(self):
         return self.name
@@ -20,3 +31,56 @@ class SvgPolygon:
             scaled_points.append((point[0] * scale_x, point[1] * scale_y))
 
         self.point_list = scaled_points
+
+    def transform(self):
+        for t_type, values in self.transformation_list:
+            len_points_list = len(self.point_list)
+            match t_type:
+                case 'translate':
+                    if len(values) == 1:
+                        for i in range (0, len_points_list):
+                            self.point_list[i] = (translate_coordinate(self.point_list[i][0], values[0]),
+                                                  self.point_list[i][1])
+                    elif len(values) == 2:
+                        for i in range(0, len_points_list):
+                            self.point_list[i] = (translate_coordinate(self.point_list[i][0], values[0]),
+                                                  translate_coordinate(self.point_list[i][1], values[1]))
+                    else:
+                        svg_polygon_logger.warning(f"unknown translate entry in values, {len(values)}")
+                case 'rotate':
+                   if len(values) == 1:
+                       for i in range (0, len_points_list):
+                           rot_x, rot_y = rotate_clockwise_around_point(self.point_list[i][0], -self.point_list[i][1], values[0], 0, 0)
+                           self.point_list[i] = (rot_x, (-1) * rot_y)
+                   elif len(values) == 3:
+                       for i in range (0, len_points_list):
+                           rot_x, rot_y = rotate_clockwise_around_point(self.point_list[i][0], -self.point_list[i][1], values[0], values[1], -values[2])
+                           self.point_list[i] = (rot_x, (-1) * rot_y)
+                   else:
+                       svg_polygon_logger.warning(f"unknown rotate entry in values, {len(values)}")
+                case 'scale':
+                    if len(values) == 1:
+                        for i in range(0, len_points_list):
+                            self.point_list[i] = (scale_coordinate(self.point_list[i][0], values[0]),
+                                scale_coordinate(self.point_list[i][1], values[0]))
+                    elif len(values) == 2:
+                        for i in range(0, len_points_list):
+                            self.point_list[i] = (scale_coordinate(self.point_list[i][0], values[0]),
+                                scale_coordinate(self.point_list[i][1], values[1]))
+                    else:
+                        svg_polygon_logger.warning(f"unknown scale entry in values, {len(values)}")
+                case 'skewX':
+                    for i in range (0, len_points_list):
+                        self.point_list[i] = (skew_x(self.point_list[i][0], self.point_list[i][1], values[0]), self.point_list[i][1])
+                case 'skewY':
+                    for i in range(0, len_points_list):
+                        self.point_list[i] = (self.point_list[i][0], skew_y(self.point_list[i][0], self.point_list[i][1], values[0]))
+                case 'matrix':
+                    if len(values) == 4:
+                        values.append(0)
+                        values.append(0)
+
+                    for i in range(0, len_points_list):
+                        p_x = values[0] * self.point_list[i][0] + values[2] * self.point_list[i][1] + values[4]
+                        p_y = values[1] * self.point_list[i][0] + values[3] * self.point_list[i][1] + values[5]
+                        self.point_list[i] = (p_x, p_y)
---
+++ tests/tests_svg-shapes/test_svgPolygon.py
@@ -0,0 +1,77 @@
+import unittest
+
+from src.svg_shapes.svgPolygon import *
+
+class TestSvgPolygon(unittest.TestCase):
+    def setUp(self):
+        self.svg_height = 800
+        self.polygon_element = {'points':"80,50 150,130 50,150"}
+
+    def test_initialization(self):
+        polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        self.assertListEqual(polygon.point_list, [(80, 750), (150, 670), (50, 650)])
+
+    def test_getter(self):
+        polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        self.assertEqual(polygon.get_name(), 'polygon')
+
+    def test_scale(self):
+        scale_x = 0.5
+        scale_y = 2
+        polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        polygon.scale(scale_x, scale_y)
+        self.assertListEqual(polygon.point_list, [(40, 1500), (75, 1340), (25, 1300)])
+
+    def test_transformation(self):
+        self.polygon_element['transform']  = 'translate(-33)'
+        x_translated_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        self.assertListEqual(x_translated_polygon.point_list, [(47, 750), (117, 670), (17, 650)])
+
+        self.polygon_element['transform'] = 'translate(12,48)'
+        translated_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        self.assertListEqual(translated_polygon.point_list, [(92, 702), (162, 622), (62, 602)])
+
+        self.polygon_element['transform'] = 'rotate(193)'
+        rotated_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        rotated_points = [(-66.70205, self.svg_height + 66.71459), (-116.91187, self.svg_height + 160.41077),
+            (-14.97585, self.svg_height + 157.40306)]
+        for i in range (0, 3):
+            self.assertAlmostEqual(rotated_polygon.point_list[i][0], rotated_points[i][0], 5)
+            self.assertAlmostEqual(rotated_polygon.point_list[i][1], rotated_points[i][1], 5)
+
+        self.polygon_element['transform'] = 'rotate(61, 263, 123)'
+        rotated_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        rotated_points = [(238.12708, self.svg_height + 72.44651), (202.09417, self.svg_height - 27.56164),
+            (136.12082, self.svg_height + 50.20414)]
+        for i in range(0, 3):
+            self.assertAlmostEqual(rotated_polygon.point_list[i][0], rotated_points[i][0], 5)
+            self.assertAlmostEqual(rotated_polygon.point_list[i][1], rotated_points[i][1], 5)
+
+        self.polygon_element['transform'] = 'scale(0.2)'
+        scaled_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        self.assertListEqual(scaled_polygon.point_list, [(16, 790), (30, 774), (10,770)])
+
+        self.polygon_element['transform'] = 'scale(5, 2)'
+        scaled_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        self.assertListEqual(scaled_polygon.point_list, [(400, 700), (750, 540), (250, 500)])
+
+        self.polygon_element['transform'] = 'skewX(-52)'
+        skew_x_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        skew_x_points = [(16.00292, 750), (-16.39241, 670), (-141.99124, 650)]
+        for i in range(0, 3):
+            self.assertAlmostEqual(skew_x_polygon.point_list[i][0], skew_x_points[i][0], 5)
+            self.assertAlmostEqual(skew_x_polygon.point_list[i][1], skew_x_points[i][1], 5)
+
+        self.polygon_element['transform'] = 'skewY(11)'
+        skew_y_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        skew_y_points = [(80, self.svg_height - 65.55042), (150, self.svg_height - 159.15705), (50, self.svg_height - 159.71902)]
+        for i in range(0, 3):
+            self.assertAlmostEqual(skew_y_polygon.point_list[i][0], skew_y_points[i][0], 5)
+            self.assertAlmostEqual(skew_y_polygon.point_list[i][1], skew_y_points[i][1], 5)
+
+        self.polygon_element['transform'] = 'matrix(2, 0.3, 2, 0.8, -32, 23)'
+        mat_polygon = SvgPolygon(self.polygon_element, self.svg_height)
+        mat_points = [(228, 713), (528, self.svg_height - 172), (368, self.svg_height - 158)]
+        for i in range(0, 3):
+            self.assertAlmostEqual(mat_polygon.point_list[i][0], mat_points[i][0], 5)
+            self.assertAlmostEqual(mat_polygon.point_list[i][1], mat_points[i][1], 5)
