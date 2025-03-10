9bdbbc9e60bccd122f1a8edeacee65b18d68e6ce
yannik <yannik.caspar@unibas.ch>
2025-03-10 14:37:49 +0100

    added transformation for svg Line and tested it

--- src/svg_shapes/svgLine.py
+++ src/svg_shapes/svgLine.py
@@ -1,15 +1,25 @@
-from src.utilities import change_svg_to_dxf_coordinate
+from src.svg_shapes.transform_messages import export_transformations
+from src.utilities import translate_coordinate, rotate_clockwise_around_point, change_svg_to_dxf_coordinate, \
+    scale_coordinate, skew_x, skew_y
+from src.logging_config import setup_logger
 
+svg_line_logger = setup_logger(__name__)
 
 class SvgLine:
 
     def __init__(self, element, svg_height):
         self.name = 'line'
         self.x1 = float(element.get('x1'))
-        self.y1 = change_svg_to_dxf_coordinate(float(element.get('y1')), svg_height)
+        self.y1 = float(element.get('y1'))
         self.x2 = float(element.get('x2'))
-        self.y2 = change_svg_to_dxf_coordinate(float(element.get('y2')), svg_height)
-        self.transform = element.get('transform')
+        self.y2 = float(element.get('y2'))
+        transform_message = element.get('transform')
+        if transform_message is not None:
+            self.transformation_list = export_transformations(transform_message)
+            self.transform(svg_height)
+
+        self.y1 = change_svg_to_dxf_coordinate(self.y1, svg_height)
+        self.y2 = change_svg_to_dxf_coordinate(self.y2, svg_height)
 
     def get_name(self):
         return self.name
@@ -19,3 +29,53 @@ class SvgLine:
         self.y1 = self.y1 * scale_y
         self.x2 = self.x2 * scale_x
         self.y2 = self.y2 * scale_y
+
+    def transform(self, svg_height):
+        for t_type, values in self.transformation_list:
+            match t_type:
+                case 'translate':
+                    self.x1 = translate_coordinate(self.x1, values[0])
+                    self.x2 = translate_coordinate(self.x2, values[0])
+                    if len(values) > 1:
+                        self.y1 = translate_coordinate(self.y1, values[1])
+                        self.y2 = translate_coordinate(self.y2, values[1])
+                case 'rotate':
+                    if len(values) == 1:
+                        self.x1, y1 = rotate_clockwise_around_point(self.x1, -self.y1, values[0], 0, 0)
+                        self.y1 = (-1) * y1
+                        self.x2, y2 = rotate_clockwise_around_point(self.x2, -self.y2, values[0], 0, 0)
+                        self.y2 = (-1) * y2
+                    elif len(values) == 3:
+                        self.x1, y1 = rotate_clockwise_around_point(self.x1, -self.y1, values[0], values[1],
+                                                                         -values[2])
+                        self.y1 = (-1) * y1
+                        self.x2, y2 = rotate_clockwise_around_point(self.x2, -self.y2, values[0], values[1],
+                                                                         -values[2])
+                        self.y2 = (-1) * y2
+                    else:
+                        svg_line_logger.warning(f"unknown values length for rotate, length: {len(values)}")
+                case 'scale':
+                    self.x1 = scale_coordinate(self.x1, values[0])
+                    self.x2 = scale_coordinate(self.x2, values[0])
+                    if len(values) == 2:
+                        self.y1 = scale_coordinate(self.y1, values[1])
+                        self.y2 = scale_coordinate(self.y2, values[1])
+                    else:
+                        self.y1 = scale_coordinate(self.y1, values[0])
+                        self.y2 = scale_coordinate(self.y2, values[0])
+                case 'skewX':
+                    self.x1 = skew_x(self.x1, self.y1, values[0])
+                    self.x2 = skew_x(self.x2, self.y2, values[0])
+                case 'skewY':
+                    self.y1 = skew_y(self.x1, self.y1, values[0])
+                    self.y2 = skew_y(self.x2, self.y2, values[0])
+                case 'matrix':
+                    if len(values) == 4:
+                        values.append(0)
+                        values.append(0)
+                    new_x1 = values[0] * self.x1 + values[2] * self.y1 + values[4]
+                    new_y1 = values[1] * self.x1 + values[3] * self.y1 + values[5]
+                    self.x1, self.y1 = new_x1, new_y1
+                    new_x2 = values[0] * self.x2 + values[2] * self.y2 + values[4]
+                    new_y2 = values[1] * self.x2 + values[3] * self.y2 + values[5]
+                    self.x2, self.y2 = new_x2, new_y2
---
+++ tests/tests_svg-shapes/test_svgLine.py
@@ -0,0 +1,98 @@
+import unittest
+
+from src.svg_shapes.svgLine import *
+
+class TestSvgLine(unittest.TestCase):
+    def setUp(self):
+        self.svg_height = 600
+        self.line_element = {'x1':"50",  'y1':"50",  'x2':"150",  'y2':"30"}
+
+    def test_initialisation(self):
+        line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(line.x1, 50)
+        self.assertEqual(line.y1, 550)
+        self.assertEqual(line.x2, 150)
+        self.assertEqual(line.y2, 570)
+
+    def test_getters(self):
+        line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(line.get_name(), 'line')
+
+    def test_scale(self):
+        scale_x = 2
+        scale_y = 3
+        line = SvgLine(self.line_element, self.svg_height)
+
+        line.scale(scale_x, scale_y)
+
+        self.assertEqual(line.x1, 100)
+        self.assertEqual(line.y1, 1650)
+        self.assertEqual(line.x2, 300)
+        self.assertEqual(line.y2, 1710)
+
+    def test_transformation(self):
+        self.line_element['transform'] = "translate(30)"
+        x_translated_line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(x_translated_line.x1, 80)
+        self.assertEqual(x_translated_line.y1, 550)
+        self.assertEqual(x_translated_line.x2, 180)
+        self.assertEqual(x_translated_line.y2, 570)
+
+        self.line_element['transform'] = "translate(-30, 60)"
+        translated_line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(translated_line.x1, 20)
+        self.assertEqual(translated_line.y1, 490)
+        self.assertEqual(translated_line.x2, 120)
+        self.assertEqual(translated_line.y2, 510)
+
+        self.line_element['transform'] = "rotate(42)"
+        rotated_line = SvgLine(self.line_element, self.svg_height)
+        self.assertAlmostEqual(rotated_line.x1, 3.70071, 5)
+        self.assertAlmostEqual(rotated_line.y1, 600-70.61377, 5)
+        self.assertAlmostEqual(rotated_line.x2, 91.39781, 5)
+        self.assertAlmostEqual(rotated_line.y2, 600-122.66394, 5)
+
+        self.line_element['transform'] = "rotate(42, 100, 250)"
+        rotated_line = SvgLine(self.line_element, self.svg_height)
+        self.assertAlmostEqual(rotated_line.x1, 196.66888, 5)
+        self.assertAlmostEqual(rotated_line.y1, 600 - 67.91450, 5)
+        self.assertAlmostEqual(rotated_line.x2, 284.36597, 5)
+        self.assertAlmostEqual(rotated_line.y2, 600 - 119.96467, 5)
+
+        self.line_element['transform'] = "scale(0.2)"
+        x_scaled_line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(x_scaled_line.x1, 10)
+        self.assertEqual(x_scaled_line.y1, 590)
+        self.assertEqual(x_scaled_line.x2, 30)
+        self.assertEqual(x_scaled_line.y2, 594)
+
+        self.line_element['transform'] = "scale(3,4)"
+        scaled_line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(scaled_line.x1, 150)
+        self.assertEqual(scaled_line.y1, 400)
+        self.assertEqual(scaled_line.x2, 450)
+        self.assertEqual(scaled_line.y2, 480)
+
+        self.line_element['transform'] = "skewX(41)"
+        skew_x_line = SvgLine(self.line_element, self.svg_height)
+        self.assertAlmostEqual(skew_x_line.x1, 93.46434, 5)
+        self.assertEqual(skew_x_line.y1, 550)
+        self.assertAlmostEqual(skew_x_line.x2, 176.07860, 5)
+        self.assertEqual(skew_x_line.y2, 570)
+
+        self.line_element['transform'] = "skewY(27)"
+        skew_y_line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(skew_y_line.x1, 50)
+        self.assertAlmostEqual(skew_y_line.y1, 600 - 75.47627, 5)
+        self.assertEqual(skew_y_line.x2, 150)
+        self.assertAlmostEqual(skew_y_line.y2, 600 - 106.42882, 5)
+
+        self.line_element['transform'] = 'matrix(3, 0.5, 0.7, 0.4, 30, 20)'
+        matrix_line = SvgLine(self.line_element, self.svg_height)
+        self.assertEqual(matrix_line.x1, 215)
+        self.assertEqual(matrix_line.y1, 535)
+        self.assertEqual(matrix_line.x2, 501)
+        self.assertEqual(matrix_line.y2, 493)
+
+
+
