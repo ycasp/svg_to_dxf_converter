4ce7a6f8c5be5cddb784c9353677ee529a009991
yannik <yannik.caspar@unibas.ch>
2025-03-11 11:42:18 +0100

    added transform() to handle transformation messages - skewX/Y not yet done, as it will not stay an ellipse

--- src/svg_shapes/svgEllipse.py
+++ src/svg_shapes/svgEllipse.py
@@ -1,5 +1,9 @@
-from src.utilities import change_svg_to_dxf_coordinate, export_rotation, rotate_clockwise_around_point
+from src.svg_shapes import export_transformations
+from src.utilities import change_svg_to_dxf_coordinate, rotate_clockwise_around_point, \
+    translate_coordinate, scale_coordinate
+from src.logging_config import setup_logger
 
+svg_ellipse_logger = setup_logger(__name__)
 
 class SvgEllipse:
 
@@ -7,21 +11,18 @@ class SvgEllipse:
         self.name = 'ellipse'
 
         self.center_x = float(element.get('cx'))
-        self.center_y = change_svg_to_dxf_coordinate(float(element.get('cy')), svg_height)
+        self.center_y = float(element.get('cy'))
 
         self.radius_x = (float(element.get('rx')), 0)
         self.radius_y = (0, float(element.get('ry')))
 
-        self.transform = element.get('transform')
-        if self.transform is not None:
-            rotation = export_rotation(self.transform)
-            if rotation is not None:
-                self.center_x, self.center_y = rotate_clockwise_around_point(self.center_x, self.center_y,
-                                        rotation[0], rotation[1], change_svg_to_dxf_coordinate(rotation[2], svg_height))
-                self.radius_x = rotate_clockwise_around_point(self.radius_x[0], self.radius_x[1],
-                                        rotation[0], 0, 0)
-                self.radius_y = rotate_clockwise_around_point(self.radius_y[0], self.radius_y[1],
-                                        rotation[0], 0, 0)
+        transform_message = element.get('transform')
+        if transform_message is not None:
+            self.transformation_list = export_transformations(transform_message)
+            self.transform()
+
+        self.center_y = change_svg_to_dxf_coordinate(self.center_y, svg_height)
+
 
 
     def get_name(self):
@@ -32,3 +33,61 @@ class SvgEllipse:
         self.center_y = self.center_y * scale_y
         self.radius_x = (self.radius_x[0] * scale_x, self.radius_x[1] * scale_y)
         self.radius_y = (self.radius_y[0] * scale_x, self.radius_y[1] * scale_y)
+
+    def transform(self):
+        for t_type, values in self.transformation_list:
+            match t_type:
+                case 'translate':
+                    if len(values) == 1:
+                        self.center_x = translate_coordinate(self.center_x, values[0])
+                    elif len(values) == 2:
+                        self.center_x = translate_coordinate(self.center_x, values[0])
+                        self.center_y = translate_coordinate(self.center_y, values[1])
+                    else:
+                        svg_ellipse_logger.warning(f"unknown translate entry in values, {len(values)}")
+                case 'rotate':
+                    if len(values) == 1:
+                        self.center_x, center_y = rotate_clockwise_around_point(self.center_x, -self.center_y,
+                                                                                     values[0], 0, 0)
+                        self.center_y = (-1) * center_y
+                        rx_x, rx_y = rotate_clockwise_around_point(self.radius_x[0], -self.radius_x[1],
+                                                                      values[0], 0, 0)
+                        self.radius_x = (rx_x, (-1) * rx_y)
+                        ry_x, ry_y = rotate_clockwise_around_point(self.radius_y[0], -self.radius_y[1],
+                                                                      values[0], 0, 0)
+                        self.radius_y = (ry_x, (-1) * ry_y)
+                    elif len(values) == 3:
+                        self.center_x, center_y = rotate_clockwise_around_point(self.center_x, -self.center_y,
+                                                                                values[0], values[1], -values[2])
+                        self.center_y = (-1) * center_y
+                        rx_x, rx_y = rotate_clockwise_around_point(self.radius_x[0], -self.radius_x[1],
+                                                                      values[0], 0, 0)
+                        self.radius_x = (rx_x, (-1) * rx_y)
+                        ry_x, ry_y  = rotate_clockwise_around_point(self.radius_y[0], -self.radius_y[1],
+                                                                      values[0], 0, 0)
+                        self.radius_y = (ry_x, (-1) * ry_y)
+                    else:
+                        svg_ellipse_logger.warning(f"unknown rotate entry in values, {len(values)}")
+                case 'scale':
+                    if len(values) == 1:
+                        self.center_x = scale_coordinate(self.center_x, values[0])
+                        self.center_y = scale_coordinate(self.center_y, values[0])
+                        self.radius_x = (scale_coordinate(self.radius_x[0], values[0]),
+                            scale_coordinate(self.radius_x[1], values[0]))
+                        self.radius_y = (scale_coordinate(self.radius_y[0], values[0]),
+                            scale_coordinate(self.radius_y[1], values[0]))
+                    elif len(values) == 2:
+                        self.center_x = scale_coordinate(self.center_x, values[0])
+                        self.center_y = scale_coordinate(self.center_y, values[1])
+                        self.radius_x = (scale_coordinate(self.radius_x[0], values[0]),
+                        scale_coordinate(self.radius_x[1], values[1]))
+                        self.radius_y = (scale_coordinate(self.radius_y[0], values[0]),
+                        scale_coordinate(self.radius_y[1], values[1]))
+                    else:
+                        svg_ellipse_logger.warning(f"unknown scale entry in values, {len(values)}")
+                case 'skewX':
+                    svg_ellipse_logger.warning(f'skewX not implemented for ellipse')
+                case 'skewY':
+                    svg_ellipse_logger.warning(f'skewY not implemented for ellipse')
+                case 'matrix':
+                    svg_ellipse_logger.warning(f'matrix not implemented for ellipse')
--- tests/tests_svg-shapes/test_svgEllipse.py
+++ tests/tests_svg-shapes/test_svgEllipse.py
@@ -4,30 +4,76 @@ from src.svg_shapes.svgEllipse import *
 
 class TestSvgEllipse(unittest.TestCase):
     def setUp(self):
-        self.normal_ellipse = {'cx':"70", 'cy':"50", 'rx':"60", 'ry':"40"}
-        self.rotated_ellipse = {'cx':"70", 'cy':"50", 'rx':"60", 'ry':"40", 'transform': 'rotate(-90)'}
-        self.svg_height = 100
+        self.svg_height = 700
+        self.normal_ellipse = {'cx':"100", 'cy':"130", 'rx':"50", 'ry':"30"}
+
 
     def test_initialization(self):
         normal_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
-        self.assertEqual(normal_ellipse.center_x, 70)
-        self.assertEqual(normal_ellipse.center_y, 50)
-        self.assertEqual(normal_ellipse.radius_x, (60, 0))
-        self.assertEqual(normal_ellipse.radius_y, (0, 40))
-        self.assertEqual(normal_ellipse.transform, None)
+        self.assertEqual(normal_ellipse.center_x, 100)
+        self.assertEqual(normal_ellipse.center_y, 570)
+        self.assertEqual(normal_ellipse.radius_x, (50, 0))
+        self.assertEqual(normal_ellipse.radius_y, (0, 30))
         self.assertEqual(normal_ellipse.name, 'ellipse')
 
-        rot_ellipse = SvgEllipse(self.rotated_ellipse, self.svg_height)
-        self.assertEqual(rot_ellipse.center_x, 50)
-        self.assertEqual(rot_ellipse.center_y, 170)
-        self.assertEqual(rot_ellipse.radius_x, (0, 60))
-        self.assertEqual(rot_ellipse.radius_y, (-40, 0))
-        self.assertEqual(rot_ellipse.transform, 'rotate(-90)')
-        self.assertEqual(rot_ellipse.name, 'ellipse')
 
     def test_getters(self):
         normal_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
         self.assertEqual(normal_ellipse.get_name(), 'ellipse')
 
-        rot_ellipse = SvgEllipse(self.rotated_ellipse, self.svg_height)
-        self.assertNotEqual(rot_ellipse.get_name(), 'circle')
+
+    def test_transform(self):
+        self.normal_ellipse['transform'] = 'translate(20)'
+        x_translated_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertEqual(x_translated_ellipse.center_x, 120)
+        self.assertEqual(x_translated_ellipse.center_y, 570)
+        self.assertEqual(x_translated_ellipse.radius_x, (50, 0))
+        self.assertEqual(x_translated_ellipse.radius_y, (0, 30))
+
+        self.normal_ellipse['transform'] = 'translate(-30, 50)'
+        translated_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertEqual(translated_ellipse.center_x, 70)
+        self.assertEqual(translated_ellipse.center_y, 520)
+        self.assertEqual(translated_ellipse.radius_x, (50, 0))
+        self.assertEqual(translated_ellipse.radius_y, (0, 30))
+
+        self.normal_ellipse['transform'] = 'rotate(61)'
+        rot_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertAlmostEqual(rot_ellipse.center_x, -65.2196, 4)
+        self.assertAlmostEqual(rot_ellipse.center_y, self.svg_height - 150.4872, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_x[0], 24.2405, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_x[1], 43.7310, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_y[0], -26.2386, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_y[1], 14.5443, 4)
+
+        self.normal_ellipse['transform'] = 'rotate(29, 120, 210)'
+        rot_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertAlmostEqual(rot_ellipse.center_x, 141.2924, 4)
+        self.assertAlmostEqual(rot_ellipse.center_y, self.svg_height - 130.3342, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_x[0], 43.7310, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_x[1], 24.2405, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_y[0], -14.5443, 4)
+        self.assertAlmostEqual(rot_ellipse.radius_y[1], 26.2386, 4)
+
+        self.normal_ellipse['transform'] = 'scale(0.6)'
+        scaled_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertEqual(scaled_ellipse.center_x, 60)
+        self.assertEqual(scaled_ellipse.center_y, 622)
+        self.assertEqual(scaled_ellipse.radius_x, (30, 0))
+        self.assertEqual(scaled_ellipse.radius_y, (0, 18))
+
+        self.normal_ellipse['transform'] = 'scale(1.4, 2.6)'
+        scaled_ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+        self.assertEqual(scaled_ellipse.center_x, 140)
+        self.assertEqual(scaled_ellipse.center_y, 362)
+        self.assertEqual(scaled_ellipse.radius_x, (70, 0))
+        self.assertEqual(scaled_ellipse.radius_y, (0, 78))
+
+        """self.normal_ellipse['transform'] = 'skewX(45)'
+        ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+
+        self.normal_ellipse['transform'] = 'skewY(33)'
+        ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)
+
+        self.normal_ellipse['transform'] = 'matrix()'
+        ellipse = SvgEllipse(self.normal_ellipse, self.svg_height)"""
